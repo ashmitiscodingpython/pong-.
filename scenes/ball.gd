02db44d924f395832ebfef6b9525b38342ca5ec7
ashmitiscodingpython <ashmit.jeevan@gmail.com>
2026-02-03 22:17:05 +0530

    Can I please stop commiting?

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -1,12 +1,13 @@
 extends RigidBody2D
 
+var velocity
+@onready var pivot = $Pivot
 
 func _ready() -> void:
-	pass
+	linear_velocity = [Vector2(-200, 200), Vector2(200, 200), Vector2(200, -200), Vector2(-200, -200)][randi_range(0, 3)]
 
 func _process(_delta: float) -> void:
-	pass
+	pivot.rotation_degrees += ((rad_to_deg(atan2(linear_velocity.y, linear_velocity.x)) + 90) - pivot.rotation_degrees) / 5
 
-func _on_body_entered(body: Node) -> void:
-	if body in get_tree().get_nodes_in_group("Paddles"):
-		linear_velocity.y += body.velocity * 50
+func _on_body_entered(_body: Node) -> void:
+	pass
--- scripts/texture_rect.gd
+++ scripts/texture_rect.gd
@@ -2,9 +2,14 @@ extends TextureRect
 
 @onready var player = $"../PaddlePlayer"
 @onready var ball = $"../Ball"
-@export var parallax = 50.0
+@export var parallax = 2.5
 var parallaxed
+var normal
+
+func _ready() -> void:
+	parallax = float(parallax)
+	normal = position
 
 func _process(_delta: float) -> void:
-	parallaxed = Vector2(ball.position.x / 576 / parallax, ball.position.y / 324 / parallax)
-	position = Vector2(-891, -477) * parallaxed
+	parallaxed = Vector2((ball.position.x / 576) / parallax, (ball.position.y / 324) / parallax)
+	position = Vector2(normal.x * parallaxed.x, normal.y * parallaxed.y)
